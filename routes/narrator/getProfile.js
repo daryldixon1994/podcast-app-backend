a21e691b0dc17ab70aede72a391049b5863f53ee
daryldixon1994 <kmohamedanouar@gmail.com>
2023-07-20 16:18:15 +0100

    adding the eoute of the narratorso he can get his own profile 2

--- routes/narrator/getProfile.js
+++ routes/narrator/getProfile.js
@@ -3,7 +3,9 @@ const Narrator = require("../../models/Narrator");
 module.exports = async (req, res) => {
   try {
     let { id } = req.params;
-    let narrator = await Narrator.findById(id);
+    let narrator = await Narrator.findById(id).select(
+      "-password -email -createdAt -updatedAt -isVerified -isNarrator -isBanned"
+    );
     if (!narrator) {
       return res.status(401).json({ status: false, message: "Wrong data" });
     }
