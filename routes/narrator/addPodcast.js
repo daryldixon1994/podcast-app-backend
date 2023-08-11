37b1d02c6c8dd862630d790e787b0c88da9e3723
daryldixon1994 <kmohamedanouar@gmail.com>
2023-08-11 15:06:26 +0100

    update adding podcast piece

--- routes/narrator/addPodcast.js
+++ routes/narrator/addPodcast.js
@@ -23,7 +23,7 @@ module.exports = async (req, res) => {
         title,
         desc,
         category,
-        tags: tags.split(","),
+        tags,
         episodeNumber,
         audioURL: `https://podcast-app-fqku.onrender.com/audios/${audio[0].filename}`,
         narrator: id,
